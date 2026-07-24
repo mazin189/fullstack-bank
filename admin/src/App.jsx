0d96f94af5248befb166d441bf33f1d58f29a671
mazin189 <mazen.ahmed.abdelattii@gmail.com>
2026-07-24 17:54:30 +0300

    test

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -23,7 +23,7 @@ export default function App() {
     ) : (
     <div className="flex">
      <Header onLogOut={logout}/>
-     <main className="flex-1">
+     <main className="">
      <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/users" element={<Users />} />
